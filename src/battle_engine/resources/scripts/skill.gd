dfc460ae230ce479d899a327aec12fc37c20b709
Luc Phan <lucvp00@gmail.com>
2025-06-15 12:24:57 -0400

    Minor changes to files from merge

--- src/battle_engine/resources/scripts/skill.gd
+++ src/battle_engine/resources/scripts/skill.gd
@@ -8,17 +8,17 @@ class_name Skill
 enum skillVariant {Heal, Buff, Debuff, TurnManip, Status}
 
 @export var name: String ## Name of the move
-@export skillVariant ##Defines the type of skill used (Heal, Stat Debuffs, Action Manipulation, Status Effects)
+@export var skillType : skillVariant ##Defines the type of skill used (Heal, Stat Debuffs, Action Manipulation, Status Effects)
 @export var cost: int ## Base skill cost
 @export var power: int ## Base strength of skill
 
 func _init(
 	name : String = "",
-	skillVariant : String = "",
+	skillType : skillVariant = skillVariant.Heal,
 	cost : int = 3, 
 	power : int = 10
 	):
 	self.name = name
-	self.skillVariant = skillVariant
+	self.skillType = skillType
 	self.cost = cost
 	self.power = power
